a1dac1b1c6823c853818782d24a92a175a0319f1
edhydev <edgar.jimenez.dev@gmail.com>
2022-05-03 08:33:44 -0500

    eureka

--- jugador-service/src/main/java/com/edgar/jugadorservice/JugadorServiceApplication.java
+++ jugador-service/src/main/java/com/edgar/jugadorservice/JugadorServiceApplication.java
@@ -2,8 +2,10 @@ package com.edgar.jugadorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class JugadorServiceApplication {
 
     public static void main(String[] args) {
